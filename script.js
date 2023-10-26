4de09a260952da648ec0dde32870ae56c3868c31
Fatima Tabassum <114339118+Fatimatabassum56@users.noreply.github.com>
2023-10-26 21:57:25 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -49,9 +49,45 @@
 
 
 
+var arr1 = [];
+var arr2 =[1,2,3,4,5,6,7];
+console.log(arr2.length);
+firstitem=arr2[0];
+console.log("First Item:", firstitem)
+var middleIndex = Math.floor(arr2.length / 2);
+var middleItem = arr2[middleIndex];
+console.log("Middle Item:", middleItem);
+var lastIndex = arr2.length - 1;
+var lastItem = arr2[lastIndex];
+console.log("Last Item:", lastItem);
+var mixDataType=["fatima",7,"shaista",8,5,7.8,true,false];
+console.log("length of mixDataType:",mixDataType.length);
+var  itCompanies = ["Facebook ", "Google ", "Microsoft ",
+    "Apple ", "IBM ", "Oracle " , "Amazon "];
+    console.log(itCompanies);
+    console.log("number of companies:",itCompanies.length);
 
 
+    firstitem=itCompanies[0];
+    console.log("First company:", firstitem)
+    var middleIndex = Math.floor(itCompanies.length / 2);
+    var middleItem = arr2[middleIndex];
+    console.log("Middle company:", middleItem);
+    var lastIndex = itCompanies.length - 1;
+    var lastItem = arr2[lastIndex];
+    console.log("Last company:", lastItem);
+    var Length = itCompanies.length;
+        for (var i = 0; i <Length; i++) {
+        console.log(itCompanies[i].toUpperCase());
+        
+    }
+    // var adding=itCompanies.splice(7, 1, and);
+    // console.log(adding);
+    var sentence = itCompanies.join("");
+    console.log(sentence,"are big IT companies");
 
+    var  itCompanies = ["Facebook ", "Google ", "Microsoft ",
+    "Apple ", "IBM ", "Oracle " , "Amazon "];
 
 
 
